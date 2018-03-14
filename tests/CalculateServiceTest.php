c0cbbceeefa11a35b2c915aa268e28c9269df016
william_ning <15237181025@163.com>
2018-03-14 14:02:51 +0800

    2

--- tests/CalculateServiceTest.php
+++ tests/CalculateServiceTest.php
@@ -9,7 +9,6 @@ use Src;
 class CalculateServiceTest extends TestCase {
     public function testAbs()
     {
-
         $ourservice = new Src\CalculateService();
         $this->assertEquals(12, $ourservice->abs(-4));
 
