52439c05f23d17a49e46ce37c1dd37d59cccb062
william_ning <15237181025@163.com>
2018-03-14 13:54:36 +0800

    测试是否成功提交

--- tests/CalculateServiceTest.php
+++ tests/CalculateServiceTest.php
@@ -6,13 +6,12 @@ use PHPUnit\Framework\TestCase;
 use Src;
 
 
-class CalculateServiceTest extends TestCase
-{
+class CalculateServiceTest extends TestCase {
     public function testAbs()
     {
 
         $ourservice = new Src\CalculateService();
-        $this->assertEquals(8, $ourservice->abs(-4));
+        $this->assertEquals(10, $ourservice->abs(-4));
 
     }
 }
